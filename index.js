de52ef04cf5a1dd3cdfd5bbfec22ad280c613634
Andrew Humphreys <ahumphreys87@googlemail.com>
2016-10-22 23:13:53 +0100

    Update dependencies

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 var transformTools =  require('browserify-transform-tools');
-var hbsCompiler = require('./compiler')
-
-
+var hbsCompiler = require('handlebars-idom');
 var options = {includeExtensions: [".hbs"]};
+
 module.exports = transformTools.makeStringTransform("hbsidomify", options,
   function (content, transformOptions, done) {
-      var file = transformOptions.file;
+    var file = transformOptions.file;
+    var result = 'var IncrementalDOM = require(\'incremental-dom\'); module.exports = function(data) {' + hbsCompiler.compile(content) + '};';
 
-      var result = 'var IncrementalDOM = require(\'incremental-dom\'); module.exports = function(data) {' + hbsCompiler.compile(content) + '};';
+    if(!transformOptions.config) {
+      return done(new Error("Could not find unbluify configuration."));
+    }
 
-      if(!transformOptions.config) {
-          return done(new Error("Could not find unbluify configuration."));
-      }
-
-      done(null, result);
+    done(null, result);
   }
 );
